7b23f95ebe2b0304be61df55f3f485be6326566b
sungwoo <sungwoo9671@naver.com>
2023-04-15 01:08:55 +0900

    reply 테이블 생성 시 삭제여부 컬럼 추가
    deleted_yn 컬럼 추가하여 삭제 여부 확인할 수 있게 함

--- src/main/java/sql/reply.sql
+++ src/main/java/sql/reply.sql
@@ -3,5 +3,6 @@ create table reply (
                        creator_id varchar(100) not null,
                        reply_content varchar(1000) not null,
                        board_idx int(10) not null,
+                       deleted_yn char(1) not null default 'N',
                        foreign key(board_idx) references t_board(board_idx)
 );
