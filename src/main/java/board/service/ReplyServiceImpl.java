b850a92ff7de624d6b6f0b25f14c94afac3948ff
sungwoo <sungwoo9671@naver.com>
2023-05-10 22:10:59 +0900

    reply 주석 추가
    reply 기능들에 주석 추가 및 사용하지 않는 코드 정리

--- src/main/java/board/service/ReplyServiceImpl.java
+++ src/main/java/board/service/ReplyServiceImpl.java
@@ -16,37 +16,37 @@ public class ReplyServiceImpl implements ReplyService {
     private ReplyMapper replyMapper;
 
     @Override
-    public List<ReplyVo> getAllReply(Integer boardIdx) {
+    public List<ReplyVo> getAllReply(Integer boardIdx) { // 댓글 조회 메서드
         return replyMapper.getAllReply(boardIdx);
     }
 
     @Override
-    public void insertReply(ReplyVo replyVo) {
+    public void insertReply(ReplyVo replyVo) { // 댓글 삽입 메서드
         replyMapper.insertReply(replyVo);
     }
 
     @Override
-    public void deleteReply(int replyIdx) {
+    public void deleteReply(int replyIdx) { // 댓글 삭제 메서드
         replyMapper.deleteReply(replyIdx);
     }
 
     @Override
-    public ReplyVo getReplyParent(String replyParent) {
+    public ReplyVo getReplyParent(String replyParent) { // 부모 댓글 정보 조회 메서드
         return replyMapper.getReplyParent(replyParent);
     }
 
     @Override
-    public void updateReplyOrder(ReplyVo replyInfo) {
+    public void updateReplyOrder(ReplyVo replyInfo) { // 댓글 출력 순서 증가 메서드
         replyMapper.updateReplyOrder(replyInfo);
     }
 
     @Override
-    public String getMaxOrder(int boardIdx) {
+    public String getMaxOrder(int boardIdx) { // 가장 최신 댓글의 순서를 구하는 메서드
         return replyMapper.getMaxOrder(boardIdx);
     }
 
     @Override
-    public void updateReplyCount(int boardIdx) {
+    public void updateReplyCount(int boardIdx) { // 댓글 갯수를 구하는 메서드
         replyMapper.updateReplyCount(boardIdx);
     }
 }
