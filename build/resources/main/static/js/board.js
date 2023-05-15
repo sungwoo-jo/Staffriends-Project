d66f6f2a92b9dc1c7fab1ab88a3b0bbb6b1dc970
sungwoo <sungwoo9671@naver.com>
2023-05-16 04:13:15 +0900

    .travis.yml 파일 branch 수정
    branch를 master -> main으로 수정

---
+++ build/resources/main/static/js/board.js
@@ -0,0 +1,73 @@
+function editAndDelete() {
+    let frm = document.getElementById("frm");
+
+    let list = document.getElementById("list");
+    let editButton = document.createElement("button"); // 수정하기 버튼 생성
+    editButton.type = 'button';
+    editButton.id = 'edit';
+    editButton.innerHTML = '수정';
+    editButton.className = 'btn btn-warning';
+    editButton.style = 'margin-top: 20px; margin-bottom: 20px;';
+    list.insertAdjacentElement("afterend", editButton);
+
+    let deleteButton = document.createElement("button"); // 삭제하기 버튼 생성
+    deleteButton.type = 'button';
+    deleteButton.id = 'delete';
+    deleteButton.innerHTML = '삭제';
+    deleteButton.className = 'btn btn-danger';
+    deleteButton.style = 'margin-top: 20px; margin-bottom: 20px;';
+    editButton.insertAdjacentElement("afterend", deleteButton);
+
+    document.getElementById("edit").onclick = function(event) {
+        frm.action = "/board/updateForm";
+        frm.submit();
+    }
+
+    document.getElementById("delete").onclick = function(event) {
+        frm.action = "/board/deleteBoard";
+        frm.submit();
+        alert('게시글 삭제가 완료되었습니다.');
+    }
+}
+
+function showBoardDetail() {
+    ClassicEditor.create( document.querySelector( '#editor' ) ) // 읽기 전용 모드
+        .then(editor => {
+            editor.enableReadOnlyMode('true');
+        })
+
+    document.getElementById("list").onclick = function(event) {
+        history.back();
+    }
+}
+
+function insertBoard() {
+    document.getElementById("save").onclick = function(event) {
+        let frm = document.getElementById("frm");
+        frm.action = "/board/insertBoard";
+        frm.submit();
+        alert('글 작성이 완료되었습니다.');
+    }
+}
+
+function writeEditor() {
+    CKEDITOR.replace('contents', {
+        filebrowserUploadUrl: '/fileUpload',
+        height: '500px'
+    });
+}
+
+function modifyBoard() {
+    let frm = document.getElementById("frm");
+    document.getElementById("successEdit").onclick = function(event) {
+        let boardIdx = document.getElementById('boardIdx');
+        frm.action = "/board/modifyBoard";
+        frm.submit();
+        alert('수정이 완료되었습니다.');
+    }
+
+    document.getElementById("list").onclick = function(event) {
+        history.back();
+        // event.preventDefault(); // 클릭 시 결과 실행 후 대기
+    }
+}
